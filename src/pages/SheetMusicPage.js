a4d88d862ecc0044d4839d438d6b74f32e089fbe
Julian Lu <j.lu@student.fontys.nl>
2020-03-22 19:16:27 +0100

    Get the sheetmusic with get request and first time trying to display PDF file, made a component for sheetmusic

--- src/pages/SheetMusicPage.js
+++ src/pages/SheetMusicPage.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import SheetMusic from '../components/SheetMusic';
+
 
 export default class SheetMusicPage extends Component {
+    state = {
+        sheetmusic: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8090/sheetmusic')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ sheetmusic: data });
+            })
+            .catch(console.log)
+    }
+
+    testFunction() {
+        var pdfAsDataUri = "data:application/pdf;base64," + this.state.sheetmusic[0].pdf;
+        console.log(pdfAsDataUri);
+        this.state.pdfAsUri = pdfAsDataUri;
+        // let pdfWindow = window.open("")
+        // pdfWindow.document.write(
+        //     "<iframe width='100%' height='100%' src='data:application/pdf;base64, " +
+        //     encodeURI(this.state.sheetmusic[0].pdf) + "'></iframe>"
+        // )
+    }
+
     render() {
         return (
             <div className="container">
                 <h1>Hello from the sheet music page</h1>
-            </div>
+                {this.state.sheetmusic.map((item) => (
+                    <SheetMusic key={item.id} sheet={item} />
+                ))}
+            </div >
         )
     }
 }
